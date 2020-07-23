8553eeeba9312e458a688744d8b2e23ba70aeacf
eunseo <es@jbnu.ac.k>
2020-07-23 21:12:00 +0900

    startclock

---
+++ src/js/clock.js
@@ -0,0 +1,13 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector(".js-title");
+
+function getTime(){
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+}
+
+function init(){
+
+}
+init();
