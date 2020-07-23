49bbc9badfb38fa4c6e30891adf763f2b29ab556
eunseo <es@jbnu.ac.k>
2020-07-24 02:08:12 +0900

    add_toDo

---
+++ src/js/todo.js
@@ -0,0 +1,33 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+function saveToDo(text){
+    localStorage.setItem(TODOS_LS, text);
+}
+
+function paintToDo(text){
+    console.log(text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    saveToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos(){
+    const toDos = localStorage.getItem(TODOS_LS);
+    if(toDos !== null){
+
+    }
+}
+function init(){
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+init();
